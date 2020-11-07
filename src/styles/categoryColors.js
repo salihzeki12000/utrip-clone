2539a14b70b7e9fc8e7c6e884515d9dd00e1e9b6
Garrett <garrett56x@gmail.com>
2020-11-07 12:29:44 -0800

    Added categoryColors file

---
+++ src/styles/categoryColors.js
@@ -0,0 +1,12 @@
+const colors = {
+  history: "#f4730f",
+  entertainment: "#ffb428",
+  art: "#6bad9b",
+  nature: "#368268",
+  relaxation: "#6498bd",
+  shopping: "#2d7a99",
+  cuisine: "#203168",
+  food: "#d96459",
+};
+
+export default colors;
