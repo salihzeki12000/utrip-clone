84cf0b4c001c843def8588e2a7bb32639d1a42b8
Garrett <garrett56x@gmail.com>
2020-11-07 11:11:16 -0800

    rendering ItemCards with photo, name, and color bar responsively

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,9 +1,24 @@
 import React from "react";
+// @ts-ignore
+import styles from "./ItemCard.module.scss";
 
 export default function ItemCard({ item }) {
+  const size = "small";
   return (
-    <div>
-      <h1>{item.name}</h1>
+    <div
+      // to={item.slug}
+      className={`${styles.itemCardWrapper} ${styles[size]}`}
+      style={{ borderColor: "#D96459" }}
+    >
+      <div
+        className={styles.itemCard}
+        style={{ backgroundImage: `url(${item.image})` }}
+      >
+        <div className={styles.itemNameWrapper}>
+          <h3 className={styles.itemName}>{item.name}</h3>
+          <p className={styles.itemPhrase}>This is a phrase</p>
+        </div>
+      </div>
     </div>
   );
 }
